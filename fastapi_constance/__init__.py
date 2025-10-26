f84b2d5f3ca543de27b75a1966b7a16d5541a9d1
Muhammad Aoun Zaidi <76547889+muhammadaounzaidi@users.noreply.github.com>
2025-10-27 00:23:56 +0500

    Enh: Renamed lifespan to constance_lifespan. (#5)

--- fastapi_constance/__init__.py
+++ fastapi_constance/__init__.py
@@ -1,5 +1,5 @@
-from fastapi_constance.lifespan import constance_config, lifespan
+from fastapi_constance.lifespan import constance_config, constance_lifespan
 from fastapi_constance.utils import register_constance_admin
 
-__all__ = ["constance_config", "lifespan", "register_constance_admin"]
+__all__ = ["constance_config", "constance_lifespan", "register_constance_admin"]
 __version__ = "0.0.1"
